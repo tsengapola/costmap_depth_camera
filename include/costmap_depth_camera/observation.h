8e99b3328a9f63d1734eae9c669b9765ee7adb39
Bilal Arain <arain.bilal@gmail.com>
2023-05-02 21:03:46 +0400

    Fixed pcl dependencies.

--- include/costmap_depth_camera/observation.h
+++ include/costmap_depth_camera/observation.h
@@ -34,9 +34,13 @@
 
 #include <geometry_msgs/msg/point32.hpp> 
 #include <sensor_msgs/msg/point_cloud2.hpp>
-#include <pcl/point_types.h>
-#include <pcl/point_cloud.h>
+//#include <pcl/point_types.h>
+//#include <pcl/point_cloud.h>
+//#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/transforms.h>
+//#include <pcl/filters/voxel_grid.h"
 #include <pcl_conversions/pcl_conversions.h>
+
 
 namespace costmap_depth_camera
 {
